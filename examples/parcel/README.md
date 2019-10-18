174cc990f04a51ef28baee7e57bbfdaefe82fb4d
Christopher Blanchard <cablanchard@gmail.com>
2019-10-18 07:43:43 +0000

    docs(Parcel): Add demo

---
+++ examples/parcel/README.md
@@ -0,0 +1,23 @@
+# Parcel Example
+
+## Use in your app
+
+### Install
+
+```bash
+npm install --save @ideal-postcodes/core-browser
+```
+
+### Usage
+
+```javascript
+import { Client } from "@ideal-postcodes/core-browser";
+```
+
+## Run this demo
+
+```bash
+npm install && npm start
+```
+
+Open [http://localhost:1234](http://localhost:1234)
---
+++ examples/parcel/main.js
@@ -0,0 +1,11 @@
+import { Client } from "@ideal-postcodes/core-browser";
+
+const client = new Client({ api_key: "iddqd" });
+
+window.addEventListener("load", async () => {
+  const postcode = "ID1 1QD";
+  const result = await client.lookupPostcode({ postcode });
+
+  const output = document.getElementById("result");
+  output.innerText = JSON.stringify(result, null, 2);
+});
