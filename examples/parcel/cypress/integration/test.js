26e005e3963ff5c9719f1810c2e4283813e41219
marcin <marcin.filip@gmail.com>
2020-01-08 13:33:46 +0100

    Cypress tests

---
+++ examples/parcel/cypress/integration/test.js
@@ -0,0 +1,15 @@
+describe("Parcel test", () => {
+  beforeEach(() => {
+    cy.visit("/index.html");
+  });
+
+  it("it pulls data from api.ideal-postcodes.co.uk", () => {
+    cy.document(document => {
+      document
+        .getElementsByTagName("ideal-postcodes-element")[0]
+        .shadowRoot.getElementById("result").innerText;
+      cy.log(text);
+      expect(JSON.stringify(text)).to.have.string("2 Barons");
+    });
+  });
+});
