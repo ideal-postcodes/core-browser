b1f1b3868eaa2474250ee46f49ba14f406d2456a
Christopher Blanchard <cablanchard@gmail.com>
2020-02-20 15:40:54 +0000

    Clean up

--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,7 @@
 import { assert } from "chai";
-//TODO change import
 import { Agent, Client } from "../lib/index";
 
-//TODO check tests
 describe("Module exports", () => {
-  it("exports Agent", () => {
-    assert.isDefined(Agent);
-  });
-  it("exports Client", () => {
-    assert.isDefined(Client);
-  });
+  it("exports Agent", () => assert.isDefined(Agent));
+  it("exports Client", () => assert.isDefined(Client));
 });
