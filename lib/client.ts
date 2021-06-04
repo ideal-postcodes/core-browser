2c19d105e53a0f657791255961f7727d8615bdfd
Christopher Blanchard <git@nf3.co>
2021-06-04 13:03:28 +0000

    feat(Core Interface): Upgrade to v2
    Version 2 of core-interface includes breaking changes to improve load
    and running time. This is achieved by reorganising the library to be
    better tree-shakeable.
    
    Helper methods (like lookupPostcode) and resources (like addresses,
    udprn) are not included with the client. Now they are exported by the
    library and require the client to be injected
    
    BREAKING CHANGE:
    
    - Package now exports a `defaults` object
    - Client.defaults has been removed
    - All client config is now stored in `client.config`
    - All resources have been removed from the client. Instead retrieve
    these from the library and inject the client. E.g.
    `client.postcodes.retrieve` becomes `postcodes.retrieve(client, ...)`
    - Helper methods (like lookupPostcode, ping) have been removed from the client.
    Instead retrieve these from teh library and inject the client. E.g.
    `client.lookupPostcode` becomes `lookupPostcode(client, ...)`

--- lib/client.ts
+++ lib/client.ts
@@ -1,43 +1,8 @@
 import {
   Client as CoreInterface,
-  TLS,
-  API_URL,
-  VERSION,
-  TIMEOUT,
-  STRICT_AUTHORISATION,
+  Config,
 } from "@ideal-postcodes/core-interface";
 import { Agent } from "./agent";
-
-export interface Config {
-  /**
-   * Use TLS. Defaults to `true`
-   */
-  tls?: boolean;
-  /**
-   * API Key. Used in API helper methods
-   */
-  api_key: string;
-  /**
-   * Target API hostname. Defaults to `'api.ideal-postcodes.co.uk'`
-   */
-  baseUrl?: string;
-  /**
-   * API version. Defaults to `'v1'`
-   */
-  version?: string;
-  /**
-   * Force autocomplete authorisation via HTTP headers only. Defaults to `false`
-   */
-  strictAuthorisation?: boolean;
-  /**
-   * Default time in ms before HTTP request timeout. Defaults to 10s (`10000`)
-   */
-  timeout?: number;
-  /**
-   * String map specifying default headers
-   */
-  header?: Record<string, string>;
-}
 
 export class Client extends CoreInterface {
   /**
@@ -49,25 +14,6 @@ export class Client extends CoreInterface {
    */
   constructor(config: Config, fetchConfig: RequestInit = {}) {
     const agent = new Agent(fetchConfig);
-    const tls = config.tls === undefined ? TLS : config.tls;
-    const baseUrl = config.baseUrl === undefined ? API_URL : config.baseUrl;
-    const version = config.version === undefined ? VERSION : config.version;
-    const strictAuthorisation =
-      config.strictAuthorisation === undefined
-        ? STRICT_AUTHORISATION
-        : config.strictAuthorisation;
-    const timeout = config.timeout === undefined ? TIMEOUT : config.timeout;
-
-    const { api_key } = config;
-    const interfaceConfig = {
-      tls,
-      api_key,
-      baseUrl,
-      version,
-      strictAuthorisation,
-      timeout,
-      header: { ...config.header },
-    };
-    super({ agent, ...interfaceConfig });
+    super({ agent, ...config });
   }
 }
